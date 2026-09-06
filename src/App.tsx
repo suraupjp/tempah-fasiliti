ec8dcef458fb2b421c6c982d1fa6d5a5f51a6a53
suraupjp <suraupjp@gmail.com>
2026-09-06 15:56:47 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/public/LandingPage';
+import StatusLookup from './pages/public/StatusLookup'; // Komponen baharu
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/semak-status" element={<StatusLookup />} />
       </Routes>
     </Router>
   );
