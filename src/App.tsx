55678c140322838a42595de4b5eed9d8013c05fa
suraupjp <suraupjp@gmail.com>
2026-09-06 18:34:37 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import LandingPage from './pages/public/LandingPage';
 import StatusLookup from './pages/public/StatusLookup';
 import AdminLogin from './pages/admin/Login';
 import Dashboard from './pages/admin/Dashboard';
-import Bookings from './pages/admin/Bookings'; // Komponen baharu
+import Bookings from './pages/admin/Bookings';
+import Facilities from './pages/admin/Facilities'; // Komponen baharu
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         <Route path="/semak-status" element={<StatusLookup />} />
         <Route path="/ajk/login" element={<AdminLogin />} />
         <Route path="/admin/dashboard" element={<Dashboard />} />
-        <Route path="/admin/bookings" element={<Bookings />} /> {/* Laluan baharu */}
+        <Route path="/admin/bookings" element={<Bookings />} />
+        <Route path="/admin/facilities" element={<Facilities />} /> {/* Laluan baharu */}
         <Route path="/admin/*" element={<Navigate to="/admin/dashboard" replace />} />
       </Routes>
     </Router>
