86b3e4ecd547868b45e49475190753483f71ffbc
suraupjp <suraupjp@gmail.com>
2026-09-06 14:56:28 +0800

    Delete src/src/lib directory

--- src/src/lib/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Nilai ini akan dimasukkan melalui Cloudflare Environment Variables nanti.
-// Ia tidak akan di-hardcode di dalam source code untuk keselamatan.
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
